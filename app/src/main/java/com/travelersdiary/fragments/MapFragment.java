e3a990e9c8f992c8e0ae36a95a1ff2658fccaf05
volfor <volfor@mail.ua>
2016-01-11 20:47:53 +0200

    added map to MapFragment

--- app/src/main/java/com/travelersdiary/fragments/MapFragment.java
+++ app/src/main/java/com/travelersdiary/fragments/MapFragment.java
@@ -3,15 +3,23 @@ package com.travelersdiary.fragments;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 import com.travelersdiary.R;
 
 import butterknife.ButterKnife;
 
 public class MapFragment extends Fragment {
+
+    private SupportMapFragment fragment;
+    private GoogleMap map;
 
     @Nullable
     @Override
@@ -20,6 +28,26 @@ public class MapFragment extends Fragment {
         ButterKnife.bind(this, view);
 
         return view;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        FragmentManager fm = getChildFragmentManager();
+        fragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+        if (fragment == null) {
+            fragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map_container, fragment).commit();
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (map == null) {
+            map = fragment.getMap();
+            map.addMarker(new MarkerOptions().position(new LatLng(0, 0)));
+        }
     }
 
 }
