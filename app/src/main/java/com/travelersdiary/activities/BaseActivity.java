f5825959d0c4c89a3cef5bcd47a00a77a31dc113
Igor Trifonov <trigor74@gmail.com>
2016-01-12 00:20:20 +0200

    Set user data to navigation drawer in progress

--- app/src/main/java/com/travelersdiary/activities/BaseActivity.java
+++ app/src/main/java/com/travelersdiary/activities/BaseActivity.java
@@ -12,14 +12,38 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
 import com.travelersdiary.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class BaseActivity extends GoogleOAuthActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    @Bind(R.id.profile_cover_image)
+    ImageView mProfileCoverImage;
+
+    @Bind(R.id.profile_image)
+    ImageView mProfileImage;
+
+    @Bind(R.id.account_name_text_view)
+    TextView mAccountName;
+
+    @Bind(R.id.account_email_text_view)
+    TextView mAccountEmail;
 
     protected NavigationView mNavigationView;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        ButterKnife.bind(this);
+    }
 
     @Override
     public void setContentView(@LayoutRes int layoutResID) {
@@ -60,17 +84,16 @@ public class BaseActivity extends GoogleOAuthActivity implements NavigationView.
     }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
     protected void setAuthenticatedUser(AuthData authData) {
         if (authData != null) {
             String name = (String) authData.getProviderData().get("displayName");
             String email = (String) authData.getProviderData().get("email");
             Uri profileImageURL = Uri.parse((String) authData.getProviderData().get("profileImageURL"));
             String UUID = authData.getUid();
+
+            mAccountName.setText(name);
+            mAccountEmail.setText(email);
+            Picasso.with(this).load(profileImageURL).into(mProfileImage);
         }
     }
 }
