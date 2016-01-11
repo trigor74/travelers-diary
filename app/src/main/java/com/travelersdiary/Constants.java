243d894fbeaef4815f0f4c3c903f24fd3cbce1f6
Igor Trifonov <trigor74@gmail.com>
2016-01-11 23:33:56 +0200

    test google auth

--- app/src/main/java/com/travelersdiary/Constants.java
+++ app/src/main/java/com/travelersdiary/Constants.java
@@ -8,4 +8,5 @@ public final class Constants {
     public static final String FIREBASE_URL = BuildConfig.FIREBASE_ROOT_URL;
     public static final String GOOGLE_API_KEY = BuildConfig.GOOGLE_API_KEY;
     public static final String GOOGLE_ANDROID_CLIENT_ID = BuildConfig.GOOGLE_ANDROID_CLIENT_ID;
+    public static final String GOOGLE_PROVIDER = "google";
 }
--- app/src/main/java/com/travelersdiary/activities/BaseActivity.java
+++ app/src/main/java/com/travelersdiary/activities/BaseActivity.java
@@ -1,7 +1,24 @@
 package com.travelersdiary.activities;
 
-import android.support.v7.app.AppCompatActivity;
+import android.net.Uri;
+import android.os.Bundle;
 
-public class BaseActivity extends AppCompatActivity {
+import com.firebase.client.AuthData;
 
+public class BaseActivity extends GoogleOAuthActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void setAuthenticatedUser(AuthData authData) {
+        if (authData != null) {
+            String name = (String) authData.getProviderData().get("displayName");
+            String email = (String) authData.getProviderData().get("email");
+            Uri profileImageURL = Uri.parse((String) authData.getProviderData().get("profileImageURL"));
+            String UUID = authData.getUid();
+        }
+    }
 }
