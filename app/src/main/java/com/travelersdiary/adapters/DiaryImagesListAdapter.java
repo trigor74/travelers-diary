b7d90dcec5b0d8f7d5c2d798af292d631326d6ab
volfor <volfor@mail.ua>
2016-02-07 14:07:53 +0200

    switched to glide

--- app/src/main/java/com/travelersdiary/adapters/DiaryImagesListAdapter.java
+++ app/src/main/java/com/travelersdiary/adapters/DiaryImagesListAdapter.java
@@ -7,7 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
-import com.squareup.picasso.Picasso;
+import com.bumptech.glide.Glide;
 import com.travelersdiary.R;
 
 import java.util.ArrayList;
@@ -48,9 +48,8 @@ public class DiaryImagesListAdapter extends RecyclerView.Adapter<DiaryImagesList
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-        Picasso.with(mContext)
+        Glide.with(mContext)
                 .load(mImagesList.get(position))
-                .resize(150, 150)
                 .centerCrop()
                 .into(holder.image);
     }
