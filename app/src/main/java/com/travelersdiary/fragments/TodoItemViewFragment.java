d691ef75e42d481847e9ac6fe08f4f7e62ba5427
TrIgor74 <trigor74@gmail.com>
2016-02-10 17:53:30 +0200

    Todo item editing in progress - added DatePickerDialog

--- app/src/main/java/com/travelersdiary/fragments/TodoItemViewFragment.java
+++ app/src/main/java/com/travelersdiary/fragments/TodoItemViewFragment.java
@@ -1,5 +1,6 @@
 package com.travelersdiary.fragments;
 
+import android.app.DatePickerDialog;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -15,6 +16,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
@@ -34,6 +36,7 @@ import com.travelersdiary.models.TodoItem;
 import org.solovyev.android.views.llm.LinearLayoutManager;
 
 import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
@@ -156,20 +159,56 @@ public class TodoItemViewFragment extends Fragment {
         super.onDestroyView();
     }
 
+    private DatePickerDialog datePicker;
+
     private void setOnClickListeners() {
         dontRemindTextView.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View v, boolean hasFocus) {
                 if (hasFocus) {
+                    dontRemindTextView.clearFocus();
                     remindTypeSpinner.requestFocus();
+                    remindTypeSpinner.performClick();
                 }
+            }
+        });
+
+        dontRemindTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dontRemindTextView.clearFocus();
+                remindTypeSpinner.requestFocus();
+                remindTypeSpinner.performClick();
             }
         });
 
         dateTextView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(getContext(), "TEST DATE", Toast.LENGTH_SHORT).show();
+                Calendar c = Calendar.getInstance();
+                long time = mTodoItem.getTime();
+                if (time > 0) {
+                    c.setTimeInMillis(time);
+                }
+                int year = c.get(Calendar.YEAR);
+                int month = c.get(Calendar.MONTH);
+                int day = c.get(Calendar.DAY_OF_MONTH);
+
+                datePicker = new DatePickerDialog(mContext, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                        Calendar c = Calendar.getInstance();
+                        long time = mTodoItem.getTime();
+                        if (time > 0) {
+                            c.setTimeInMillis(time);
+                        }
+                        c.set(year, monthOfYear, dayOfMonth);
+                        mTodoItem.setTime(c.getTimeInMillis());
+                        dateTextView.setText(SimpleDateFormat.getDateTimeInstance().format(mTodoItem.getTime()));
+                    }
+                }, year, month, day);
+
+                datePicker.show();
             }
         });
 
