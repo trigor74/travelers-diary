ccda465270d5316318ba09d1d7083d5cc3b0bb02
volfor <volfor@mail.ua>
2016-02-19 16:41:48 +0200

    cleanup

--- app/src/main/java/com/travelersdiary/activities/FullScreenImageActivity.java
+++ app/src/main/java/com/travelersdiary/activities/FullScreenImageActivity.java
@@ -9,6 +9,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 import com.travelersdiary.R;
 import com.travelersdiary.Utils;
@@ -21,7 +22,7 @@ import butterknife.ButterKnife;
 
 public class FullScreenImageActivity extends AppCompatActivity {
 
-    @Bind(R.id.full_screen_image_activity2_toolbar)
+    @Bind(R.id.full_screen_image_activity_toolbar)
     Toolbar mToolbar;
 
     @Bind(R.id.vp_image)
@@ -70,6 +71,9 @@ public class FullScreenImageActivity extends AppCompatActivity {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Utils.setStatusBarColor(this, ContextCompat.getColor(this, R.color.colorPrimaryDark));
         }
+
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        mSupportActionBar.hide();
 
         setupViewPager();
         setActionBarTitle(mPosition);
