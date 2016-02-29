863bf654edf6e34ea3f5c25f07883abce3342bd4
volfor <volfor@mail.ua>
2016-02-29 23:32:44 +0200

    crashlytics

--- app/src/main/java/com/travelersdiary/application/Application.java
+++ app/src/main/java/com/travelersdiary/application/Application.java
@@ -1,12 +1,16 @@
 package com.travelersdiary.application;
 
+import com.crashlytics.android.Crashlytics;
 import com.firebase.client.Firebase;
 import com.onegravity.rteditor.fonts.FontManager;
+import io.fabric.sdk.android.Fabric;
 
 public class Application extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        Fabric.with(this, new Crashlytics());
+        
         Firebase.setAndroidContext(this);
         // enable disk persistence
         Firebase.getDefaultConfig().setPersistenceEnabled(true);
