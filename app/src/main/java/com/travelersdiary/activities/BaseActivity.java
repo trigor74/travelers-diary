97a6bc89cdaeb52f632c1daabbe4f51b2174ccb2
volfor <volfor@mail.ua>
2016-04-14 17:25:22 +0300

    start-stop tracking in single button, stop tracking when travel finished

--- app/src/main/java/com/travelersdiary/activities/BaseActivity.java
+++ app/src/main/java/com/travelersdiary/activities/BaseActivity.java
@@ -13,6 +13,7 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.FrameLayout;
@@ -41,6 +42,7 @@ public class BaseActivity extends AppCompatActivity implements
         NavigationView.OnNavigationItemSelectedListener {
 
     protected NavigationView mNavigationView;
+    private Menu mMenu;
     private DrawerLayout mDrawerLayout;
     private ActionBarDrawerToggle mDrawerToggle;
 
@@ -221,6 +223,7 @@ public class BaseActivity extends AppCompatActivity implements
         ButterKnife.bind(this);
 
         mNavigationView = (NavigationView) findViewById(R.id.nav_view);
+        mMenu = mNavigationView.getMenu();
     }
 
     public void setupNavigationView(Toolbar toolbar) {
@@ -280,34 +283,10 @@ public class BaseActivity extends AppCompatActivity implements
                 startActivity(intent);
                 return true;
             case R.id.nav_start_tracking:
-                SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                String activeTravelKey = sharedPreferences.getString(Constants.KEY_ACTIVE_TRAVEL_KEY, null);
-                if (Constants.FIREBASE_TRAVELS_DEFAULT_TRAVEL_KEY.equals(activeTravelKey)) {
-                    if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
-                        mDrawerLayout.closeDrawers();
-                    }
-                    new AlertDialog.Builder(this)
-                            .setInverseBackgroundForced(true)
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .setTitle(getString(R.string.dialog_cannot_tracking_title_text))
-                            .setMessage(getString(R.string.dialog_cannot_tracking_message_text))
-                            .setPositiveButton(R.string.close, new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    // nothing
-                                }
-                            })
-                            .show();
-                    return true;
-                }
-                Intent intentStartTracking = new Intent(this, LocationTrackingService.class);
-                intentStartTracking.setAction(LocationTrackingService.ACTION_START_TRACK);
-                startService(intentStartTracking);
+                startTracking();
                 return true;
             case R.id.nav_stop_tracking:
-                Intent intentStopTracking = new Intent(this, LocationTrackingService.class);
-                intentStopTracking.setAction(LocationTrackingService.ACTION_STOP_TRACK);
-                startService(intentStopTracking);
+                stopTracking();
                 return true;
             case R.id.nav_settings:
                 intent = new Intent(this, PreferencesActivity.class);
@@ -346,5 +325,48 @@ public class BaseActivity extends AppCompatActivity implements
 
     public boolean isDrawerOpen() {
         return mDrawerLayout.isDrawerOpen(GravityCompat.START);
+    }
+
+    public void startTracking() {
+        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        String activeTravelKey = sharedPreferences.getString(Constants.KEY_ACTIVE_TRAVEL_KEY, null);
+        if (Constants.FIREBASE_TRAVELS_DEFAULT_TRAVEL_KEY.equals(activeTravelKey)) {
+            if (mDrawerLayout.isDrawerOpen(GravityCompat.START)) {
+                mDrawerLayout.closeDrawers();
+            }
+            new AlertDialog.Builder(this)
+                    .setInverseBackgroundForced(true)
+                    .setIcon(android.R.drawable.ic_dialog_alert)
+                    .setTitle(getString(R.string.dialog_cannot_tracking_title_text))
+                    .setMessage(getString(R.string.dialog_cannot_tracking_message_text))
+                    .setPositiveButton(R.string.close, new DialogInterface.OnClickListener() {
+                        @Override
+                        public void onClick(DialogInterface dialog, int which) {
+                            // nothing
+                        }
+                    })
+                    .show();
+
+            switchStartStop(false);
+            return;
+        }
+        Intent intentStartTracking = new Intent(this, LocationTrackingService.class);
+        intentStartTracking.setAction(LocationTrackingService.ACTION_START_TRACK);
+        startService(intentStartTracking);
+
+        switchStartStop(true);
+    }
+
+    public void stopTracking() {
+        Intent intentStopTracking = new Intent(this, LocationTrackingService.class);
+        intentStopTracking.setAction(LocationTrackingService.ACTION_STOP_TRACK);
+        startService(intentStopTracking);
+
+        switchStartStop(false);
+    }
+
+    private void switchStartStop(boolean isStarted) {
+        mMenu.findItem(R.id.nav_start_tracking).setVisible(!isStarted);
+        mMenu.findItem(R.id.nav_stop_tracking).setVisible(isStarted);
     }
 }
